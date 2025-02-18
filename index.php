f751d95a41f6c364f3c2c0330f9cb7a2df7b25d9
Thibault-Du <thibault.ducruet@etu.univ-lyon1.fr>
2025-02-18 16:25:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset='UTF-8'>
+
+    <title>Le Réseau Polytech - Réseau Polytech</title>
+    <meta name="description"
+        content="Le réseau Polytech regroupe 16 écoles d’ingénieurs et 4 écoles associées. Toutes sont des écoles publiques universitaires relevant du ministère de l’Enseignement supérieur, de la Recherche et de l’Innovation et délivrant des diplômes d’ingénieur habilités par la Commission des Titres d’Ingénieur (CTI). Avec 12 domaines de formation d’ingénieur, plus de 100 000 diplômés et 3 800 nouveaux par an, soit 10 % des ingénieurs diplômés en France, les écoles Polytech constituent un des plus grands réseaux français de formation d’ingénieurs." />
+    <link rel="canonical" href="https://www.polytech-reseau.org/" />
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Le Réseau Polytech - Réseau Polytech" />
+    <meta property="og:description"
+        content="Le réseau Polytech regroupe 16 écoles d’ingénieurs et 4 écoles associées. Toutes sont des écoles publiques universitaires relevant du ministère de l’Enseignement supérieur, de la Recherche et de l’Innovation et délivrant des diplômes d’ingénieur habilités par la Commission des Titres d’Ingénieur (CTI). Avec 12 domaines de formation d’ingénieur, plus de 100 000 diplômés et 3 800 nouveaux par an, soit 10 % des ingénieurs diplômés en France, les écoles Polytech constituent un des plus grands réseaux français de formation d’ingénieurs." />
+    <meta property="og:url" content="https://www.polytech-reseau.org/" />
+    <meta property="og:site_name" content="Réseau Polytech" />
+    <meta property="article:modified_time" content="2024-10-16T14:00:53+00:00" />
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div class="wrapper ">
+        <header class="header header-with-topbar">
+            <nav class="navbar">
+                <div class="container-header">
+                    <a class="navbar-brand" href="index" title="Réseau Polytech">
+                        <img src="image/cropped-logo_reseau_Polytech.webp"
+                            alt="Réseau Polytech">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a href="/le-reseau-polytech/" class="nav-link">Le réseau</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="https://www.polytech-reseau.org/le-reseau-polytech/">Découvrir le réseau
+                                        Polytech</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nos-ecoles-ingenieurs/">Nos écoles</a></li>
+                                <li><a href="https://www.polytech-reseau.org/la-vie-etudiante/">La vie étudiante</a>
+                                </li>
+                                <li><a href="https://www.polytech-reseau.org/la-fondation/">La Fondation</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nos-valeurs/">Nos valeurs</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nos-engagements/">Nos engagements</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nos-projets/">Nos projets</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nous-rencontrer/">Nous rencontrer</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nos-actualites/">Nos actualités</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="/formations/" class="nav-link">Formations</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="https://www.polytech-reseau.org/le-cycle-preparatoire-peip/">Le cycle
+                                        préparatoire PeiP</a></li>
+                                <li><a href="https://www.polytech-reseau.org/le-cycle-ingenieur/">Le cycle ingénieur</a>
+                                <li><a href="https://www.polytech-reseau.org/12-domaines-de-formation/">12 domaines de
+                                        formation</a></li>
+                                <li><a href="https://www.polytech-reseau.org/nos-diplomes/">Nos diplômes</a></li>
+                                <li><a href="https://www.polytech-reseau.org/les-alternatives-au-cursus/">Les
+                                        alternatives au cursus</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a href="#" class="nav-link">Postuler à Polytech</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Entreprises</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">International</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Réussir avec Polytech</a></li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link">A propos de nous</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="newsletter">Newsletter</a>
+                                </li>
+                                <li><a href="Contactez_nous">Nous contacter</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </header>
+
+
+        <div class="header-filter">
+            <div>
+                <h0 class="title">Le réseau Polytech</h0>
+                <br>
+                <span class="sub-title">Le réseau français des écoles d'ingenieurs
+                    polytechniques des universités</span>
+                <br>
+                <br>
+                <a href="/rechercher-une-formation-ingenieur-polytech/"
+                    title="Rechercher une formation d&#039;ingénieur" class="btn-polytech">Rechercher une
+                    formation d&#039;ingénieur</a>
+            </div>
+        </div><!-- /.container -->
+
+
+        <div class="main  main-raised ">
+            <div class="main-container">
+                <br>
+                <h2 class="title2">Découvrir notre réseau</h2>
+                <p class="description">
+                    Le réseau Polytech regroupe 16 écoles d’ingénieurs et 5 écoles
+                    associées. Toutes sont des écoles publiques universitaires relevant du ministère de
+                    l’Enseignement supérieur, de la Recherche et de l’Innovation et délivrant des diplômes
+                    d’ingénieur habilités par la Commission des Titres d’Ingénieur (CTI). Avec 12 domaines
+                    de formation d’ingénieur, plus de 100 000 diplômés et 4 100 nouveaux par an, soit 10 %
+                    des ingénieurs diplômés en France, les écoles Polytech constituent un des plus grands
+                    réseaux français de formation d’ingénieurs. Le Parcours des écoles d'ingénieurs Polytech
+                    (PeiP), accessible via leconcours Geipi Polytech, permet d’accéder directement à un
+                    cycle ingénieur Polytech en formation initiale.
+                </p>
+
+                <br>
+                <div class="container">
+                    <table class="features-table">
+                        <tr>
+                            <td class="features-column">
+                                <div class="feature">
+                                    <a href="/nos-ecoles-ingenieurs/">
+                                        <h4>16 écoles d'ingénieurs publiques</h4>
+                                    </a>
+                                </div>
+                                <div class="feature">
+                                    <a href="/12-domaines-de-formation/">
+                                        <h4>12 domaines de formation</h4>
+                                    </a>
+                                </div>
+                                <div class="feature">
+                                    <a href="/rechercher">
+
+                                            <h4>100 spécialités</h4>
+                                    </a>
+                                </div>
+                            </td>
+                            <td class="map-column">
+                                <a href="https://www.polytech-reseau.org/nos-ecoles-ingenieurs/"><img
+                                        src="image\carte-polytech.webp" alt="Carte" class="map-image"></a>
+                            </td>
+                            <td class="features-column">
+                                <div class="feature">
+                                    <a href="/polytech-alumni/">
+                                        <h4>100 000 diplômés</h4>
+                                    </a>
+                                </div>
+                                <div class="feature">
+                                    <a href="/entreprises/">
+                                        <h4>Entreprises</h4>
+                                    </a>
+                                </div>
+                                <div class="feature">
+                                    <a href="/international/">
+                                        <h4>International</h4>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="box">
+                    <h2>Témoignages</h2>
+                    <p>Découvrez les retours d’expérience de notre communauté en un clic.</p>
+                    <a href="/rechercher-une-formation-ingenieur-polytech/" class="btn-polytech">Découvrez nos
+                        témoignages</a>
+                </div>
+                <div class="box">
+                    <h2>Actualités</h2>
+                    <p>Restez informé des dernières nouveautés en consultant notre page dédiée aux actualités.</p>
+                    <a href="/rechercher-une-formation-ingenieur-polytech/" class="btn-polytech">Toutes nos
+                        actualités</a>
+                </div>
+                <div class="container">
+                    <img src="image\commission.webp"></img>
+                </div>
+            </div>
+            <footer class="footer">
+                <div class="container">
+                    <ul class="footer-menu">
+                        <li><a href="/actualites">Actualités</a></li>
+                        <li><a href="/brochures">Brochures</a></li>
+                        <li><a href="/presse">Presse</a></li>
+                        <li><a target="_blank" href="https://cloud.polytech-reseau.org/">Intranet</a></li>
+                        <li><a href="https://eplanet.polytech-reseau.org">E.planet</a></li>
+                        <li><a href="/mentions">Mentions légales</a></li>
+                        <li><a href="https://www.ecoles-ingenieurs.fr/">Témoignages</a></li>
+                        <li><a href="https://www.polytech-reseau.org/foire-aux-questions-polytech">Foire aux
+                                questions</a>
+                        </li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
+
+</body>
+
+</html>
